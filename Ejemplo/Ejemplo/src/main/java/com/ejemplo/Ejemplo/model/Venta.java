d2e5fcd0cd1c04140cf864fc112328a9fb8de5de
Brenda <brentorres26@gmail.com>
2024-04-22 22:52:17 -0300

    Cambio en atributos Venta

--- Ejemplo/Ejemplo/src/main/java/com/ejemplo/Ejemplo/model/Venta.java
+++ Ejemplo/Ejemplo/src/main/java/com/ejemplo/Ejemplo/model/Venta.java
@@ -8,49 +8,20 @@ public class Venta {
     private Integer plantaId;
     private Integer cantidad;
     private Date fechaVenta;
-    private Date fechaBaja;
-    private String descripcion;
 
     public Venta() {
+    	
     }
 
-   
-
-    public Venta(Integer id, Integer plantaId, Integer cantidad, Date fechaVenta, Date fechaBaja, String descripcion) {
+    public Venta(Integer id, Integer plantaId, Integer cantidad, Date fechaVenta) {
 		super();
 		this.id = id;
 		this.plantaId = plantaId;
 		this.cantidad = cantidad;
 		this.fechaVenta = fechaVenta;
-		this.fechaBaja = fechaBaja;
-		this.descripcion = descripcion;
 	}
 
 	// Getters y Setters
-
-    public Date getFechaBaja() {
-		return fechaBaja;
-	}
-
-
-
-	public void setFechaBaja(Date fechaBaja) {
-		this.fechaBaja = fechaBaja;
-	}
-
-
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-
 
 	public Integer getId() {
         return id;
